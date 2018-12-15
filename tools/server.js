4dbb44a87a97b43e23ba0aa524ea8d90656a9386
omotola28 <omotolashogunle@gmail.com>
2018-12-15 09:07:42 +0000

    removed comment from server

--- tools/server.js
+++ tools/server.js
@@ -14,10 +14,10 @@ const auth ={
     domain: 'sandbox773c0b63d36548ce980f5c000059eaf7.mailgun.org'
   }
 }
-//import cors from 'cors';
+
 
 const nodemailerMailgun = nodemailer.createTransport(nodemailerTransport(auth));
-/* eslint-disable no-console */
+
 
 const port = 3000;
 const app = express();
@@ -48,60 +48,19 @@ app.post('/inbox', (req,res) => {
   subject: req.body.subject,
 
   html: `<p>${req.body.message}</p>`,
-    //You can use "text:" to send plain-text content. It's oldschool!
   text: 'Mailgun rocks, pow pow!'
-  //You can use "text:" to send plain-text content. It's oldschool!
   }, function (err, info) {
   if (err) {
     console.log('Error: ' + err);
-    //next(err);
       res.json(err);
-     //res.send(err);
   }
   else {
     console.log('Response: ' + info);
-    //res.render('/inbox', {msg:'Sent'});
     res.json(info);
   }
   });
 
-    /*//console.log(req.body);
-      const htmlEmail = `
-            <h3>Contact Details</h3>
-            <ul>
-              <li> Name ${req.body.name}</li>
-              <li> Email ${req.body.email}</li>
-            </ul>
-            <h3>Message</h3>
-            <p>${req.body.message}</p>
-      `
-
-      const transporter = nodemailer.createTransport({
-          host: 'mxb.mailgun.org.s',
-          port: 587,
-          auth: {
-              user: 'hw5o6shamn4tqjlx@ethereal.email',
-              pass: 'asFQPK2p1Qpqe95SKG'
-          }
-      })
-
-      let mailOptions = {
-               from: req.body.email,
-               to: 'hw5o6shamn4tqjlx@ethereal.email', // list of receivers
-               subject: req.body.subject, // Subject line
-               text: req.body.message,
-               html: htmlEmail
-      };
-
-      transporter.sendMail(mailOptions, (error, info) => {
-              if (error) {
-                  return console.log(error);
-              }
-              console.log('Message sent: %s', info.message);
-              
-              console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-          });*/
+   
 })
 
 
